3619f7368ae2177d1874e18ae7aee878cd62c8d8
Alec Schleicher <alecs@live.unc.edu>
2017-06-26 03:25:10 -0400

    finished pausing/resuming gifs

--- app.js
+++ app.js
@@ -15,21 +15,16 @@ $(document).ready(function(){
             makeButton(text);
         }
     });
-
-    //Respond to gif button clicks
-    $(".gif-button").click(function(){
-        getGif($(this).text());
-        console.log($(this).text());
-        console.log("test");
-    })
 });
 
 function makeButton(text) {
         var button = $("<button>");
         button.html(text);
         button.addClass("btn btn-primary gif-button");
+        button.click(function(){
+            getGif($(this).text());
+        })
         $("#buttons").append(button);  
-        console.log(text);
 }
 
 function getGif(search) {
@@ -41,7 +36,7 @@ function getGif(search) {
         url: queryURL,
         method: "GET"
     }).done(function(response) {
-
+        console.log(response);
         var gifs = response.data;
 
         for (i=0; i<gifs.length; i++) {
@@ -49,7 +44,22 @@ function getGif(search) {
             var container = $("<div>").addClass("img text-center");
 
             //Append Gif and rating
-            var gif = $("<img>").attr("src", gifs[i]["images"]["downsized"]["url"]);
+            var gif = $("<img>").attr("src", gifs[i]["images"]["downsized_still"]["url"]);
+            gif.attr("data-state", "still");
+            gif.attr("data-still", gifs[i]["images"]["downsized_still"]["url"]);
+            gif.attr("data-animate", gifs[i]["images"]["downsized"]["url"]);
+            gif.click(function() {
+                var state = $(this).attr("data-state");
+                console.log($(this));
+                if (state === "still") {
+                    $(this).attr("src", $(this).attr("data-animate"));
+                    $(this).attr("data-state", "animated");
+                } else if (state !== "still") {
+                    $(this).attr("src", $(this).attr("data-still"));
+                    $(this).attr("data-state", "still");
+                }
+
+            });
             container.append("Rating: " + gifs[i]["rating"])
             container.append(gif);
             $("#gifs").prepend(container);
