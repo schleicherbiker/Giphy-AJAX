4a8660034329dbf495312cf99f9cb24086063942
Alec Schleicher <schleicher@3cisd.com>
2017-06-25 22:26:47 -0400

    added new button functionality

--- app.js
+++ app.js
@@ -1,26 +1,42 @@
-//Declare topics
-var topics = ["Spongebob", "Patrick", "Rick and Morty", "Peter Griffin", "Stewie Griffin", "Quagmire"];
-console.log("test");
+$(document).ready(function(){
+    //Declare topics
+    var topics = ["Spongebob", "Patrick", "Rick and Morty", "Peter Griffin", "Stewie Griffin", "Quagmire"];
 
-//Make buttons
-for (i=0; i<topics.length; i++) {
-    var button = $("<button>");
-    button.html(topics[i]);
-    button.addClass("btn-primary");
-    button.click(function(){
+    //Make initial buttons
+    for (i=0; i<topics.length; i++) {
+        makeButton(topics[i]);
+    }
+
+    //Make form for new button
+    $("#addGif").click(function() {
+        text = $("input").val();
+        if (text !== "") {
+            $("input").val("");
+            makeButton(text);
+        }
+    });
+
+    //Respond to gif button clicks
+    $(".gif-button").click(function(){
         getGif($(this).text());
+        console.log($(this).text());
+        console.log("test");
     })
-    $("#buttons").append(button);
+});
+
+function makeButton(text) {
+        var button = $("<button>");
+        button.html(text);
+        button.addClass("btn btn-primary gif-button");
+        $("#buttons").append(button);  
+        console.log(text);
 }
-
-
-
 
 function getGif(search) {
 
     var apiKey = "78fec19e2d8242fbae167723b8736325";
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=" + apiKey + "&limit=10";
-    console.log(queryURL);
+  
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -36,9 +52,7 @@ function getGif(search) {
             var gif = $("<img>").attr("src", gifs[i]["images"]["downsized"]["url"]);
             container.append("Rating: " + gifs[i]["rating"])
             container.append(gif);
-
             $("#gifs").prepend(container);
-
         }
     })
 }
